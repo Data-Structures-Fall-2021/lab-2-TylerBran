60fef622cd9ea1b45c2e226828242e28ec184a62
TylerBran <90166780+TylerBran@users.noreply.github.com>
2021-09-26 16:31:30 -0400

    Update Song.java

--- src/main/java/Song.java
+++ src/main/java/Song.java
@@ -1,5 +1,45 @@
 public class Song {
+    private String artist;
+    private String title;
+    private int duration;
+    public static String collectionName;
 
-	// YOUR CODE GOES HERE
-	
+    public Song(String artistInput, String titleInput, int durationInput){
+        this.artist = artistInput;
+        this.title = titleInput;
+        this.duration = durationInput;
+    }
+    private String getArtist() {
+        return artist;
+    }
+    private void setArtist(String artist) {
+        this.artist = artist;
+    }
+    private String getTitle() {
+        return title;
+    }
+    private void setTitle(String title) {
+        this.title = title;
+    }
+    private int getDuration() {
+        return duration;
+    }
+    private void setDuration(int duration) {
+        this.duration = duration;
+    }
+    public String getCollectionName() {
+        return collectionName;
+    }
+    public void collectionName(String collectionName) {
+        Song.collectionName = collectionName;
+    }
+    public String getMinutes(int duration){
+        int temp = duration;
+        int minutes = temp / 60;
+        int seconds = temp % 60;
+        return String.format("%d:%d", minutes, seconds);
+    }
+    public String toString(){
+        return this.title + " (" + this.artist + ") - " + getMinutes(this.duration);
+    }
 }
