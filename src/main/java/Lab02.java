c4a4933f265fe7c5c54d828d9ee78905e26250ac
TylerBran <90166780+TylerBran@users.noreply.github.com>
2021-09-26 16:26:11 -0400

    Update Lab02.java

--- src/main/java/Lab02.java
+++ src/main/java/Lab02.java
@@ -1,5 +1,16 @@
 public class Lab02 {
+import java.util.Arrays;
 
-	// YOUR CODE GOES HERE
-
+public class Lab02 {
+    public static double[] map(double [] NArray, UnaryFunction ufunction){
+        double[] results = new double[NArray.length];
+        int i=0;
+        for (double N : NArray){
+            results[i] = ufunction.exec(N);
+            i++;
+        }
+        System.out.println(Arrays.toString(results));
+        return results;
+    }
+}
 }
